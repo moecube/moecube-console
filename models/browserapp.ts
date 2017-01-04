e3beef237d40af9c622aeab48cf23904e287c2a2
zh99998 <zh99998@gmail.com>
2017-01-04 15:28:09 +0800

    页面

--- models/browserapp.ts
+++ models/browserapp.ts
@@ -12,16 +12,13 @@ class App {
     news: I18n<{title: string, url: string, image: string}[]>;
     conference?: string;
     data: any;
-}
 
-export class BrowserApp {
-    id: string;
-    name: I18n<string>;
-    parent?: string;
-    locales: string[];
-    news: I18n<{title: string, url: string, image: string}[]>;
-    conference?: string;
-    data: any;
+    // TODO: 实现进 Model 里
+    constructor(o: any) {
+        for (let [key, value] of Object.entries(o)) {
+            this[key] = value;
+        }
+    }
 }
 
 export default App;
