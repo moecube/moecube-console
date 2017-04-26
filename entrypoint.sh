43ec16b56cf291a63a8c468dcff640ce85f1a2f5
nano <ncysatnaf@gmail.com>
2017-04-26 18:23:30 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 
 aria2c --conf-path=/usr/src/app/aria2.conf -D;
+
+./ossutil config --endpoint oss-cn-hangzhou.aliyuncs.com --access-key-id $OSS_ACCESS_ID
+  --access-key-secret $OSS_ACCESS_KEY;
+
 npm start
